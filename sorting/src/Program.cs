7c1aaa5caf70cad9c51064e2f7282b98d2cf533d
Charles Pascoe <cpascoe95@gmail.com>
2016-09-29 11:29:39 +0100

    Demo of sorting

--- sorting/src/Program.cs
+++ sorting/src/Program.cs
@@ -1,7 +1,43 @@
-namespace Sorting {
-    public static class Programming {
-        public static void Main(string [] args) {
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Sorting.Sorters;
 
+namespace Sorting {
+    public static class Program {
+        public static void Main(string [] args) {
+            ISorter insertionSorter = new InsertionSorter();
+
+            List<int> items = new List<int> {
+                5, 9, 1, 12, 7, 9, 2, 3, 99, 0
+            };
+
+            Program.PrintList(items);
+
+            insertionSorter.Sort(items);
+
+            Program.PrintList(items);
+        }
+
+        public static void PrintList<T>(IList<T> items) {
+            StringBuilder str = new StringBuilder();
+            str.Append("[");
+
+            bool firstItem = true;
+
+            foreach (T item in items) {
+                if (!firstItem) {
+                    str.Append(", ");
+                } else {
+                    firstItem = false;
+                }
+
+                str.Append(item);
+            }
+
+            str.Append("]");
+
+            Console.WriteLine(str);
         }
     }
 }
